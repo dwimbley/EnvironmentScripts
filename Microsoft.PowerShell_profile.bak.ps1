0c2635c2f93bc868e89deec8140a50d9db7cf72e
David Wimbley <dwimbley@outlook.com>
2019-12-04 23:54:29 -0500

    Update Microsoft.PowerShell_profile.bak.ps1

--- Microsoft.PowerShell_profile.bak.ps1
+++ Microsoft.PowerShell_profile.bak.ps1
@@ -1,5 +1,6 @@
 
-. 'C:\Users\dwimbley\Documents\WindowsPowerShell\Modules\posh-git\0.6.1.20160330\profile.example.ps1'
-. "C:\Users\dwimbley\Documents\WindowsPowerShell\Modules\get-childitem-color\get-childitem-color.ps1"
+Import-Module posh-git
+Import-Module Get-ChildItemColor
+
 Set-Alias l Get-ChildItem-Color -option AllScope
-Set-Alias ls Get-ChildItem-Format-Wide -option AllScope
+Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
