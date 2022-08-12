ec316ca697b045297d42eb2b675eaa139b61519e
David Wimbley <dwimbley@outlook.com>
2022-08-12 01:51:49 -0400

    Update Microsoft.PowerShell_profile.bak.ps1

--- Microsoft.PowerShell_profile.bak.ps1
+++ Microsoft.PowerShell_profile.bak.ps1
@@ -4,3 +4,12 @@ Import-Module Get-ChildItemColor
 
 Set-Alias l Get-ChildItem-Color -option AllScope
 Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
+
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+
+(@(& 'C:/Users/David/AppData/Local/Programs/oh-my-posh/bin/oh-my-posh.exe' init pwsh --config='C:\Users\David\AppData\Local\Programs\oh-my-posh\themes\jandedobbeleer.omp.json' --print) -join "`n") | Invoke-Expression
+
